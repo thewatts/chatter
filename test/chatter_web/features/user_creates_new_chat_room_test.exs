22443f89ecaed305a864335a4aa03b7026312bc1
Nathaniel Watts <1141717+thewatts@users.noreply.github.com>
2021-01-24 14:35:00 -0600

    Refactoring

--- test/chatter_web/features/user_creates_new_chat_room_test.exs
+++ test/chatter_web/features/user_creates_new_chat_room_test.exs
@@ -3,10 +3,23 @@ defmodule ChatterWeb.UserCreatesNewChatRoomTest do
 
   test "user creates a new chat room successfully", %{session: session} do
     session
-    |> visit(Routes.chat_room_path(@endpoint, :index))
-    |> click(Query.link("New chat room"))
-    |> fill_in(Query.text_field("Name"), with: "elixir")
+    |> visit(rooms_index())
+    |> click(new_chat_link())
+    |> create_chat_room(name: "elixir")
+    |> assert_has(room_title("elixir"))
+  end
+
+  defp rooms_index, do: Routes.chat_room_path(@endpoint, :index)
+
+  defp new_chat_link, do: Query.link("New chat room")
+
+  defp create_chat_room(session, name: name) do
+    session
+    |> fill_in(Query.text_field("Name"), with: name)
     |> click(Query.button("Submit"))
-    |> assert_has(Query.data("role", "room-title", text: "elixir"))
+  end
+
+  defp room_title(title) do
+    Query.data("role", "room-title", text: title)
   end
 end
