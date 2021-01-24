b16df0099a23144ad1c8b27159576c9f934254ea
Nathaniel Watts <1141717+thewatts@users.noreply.github.com>
2021-01-23 22:07:21 -0600

    Initial generation + wallaby setup + smoke test

---
+++ config/test.exs
@@ -0,0 +1,26 @@
+use Mix.Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :chatter, Chatter.Repo,
+  database: "chatter_test#{System.get_env("MIX_TEST_PARTITION")}",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :chatter, ChatterWeb.Endpoint,
+  http: [port: 4002],
+  server: true
+
+# Only use Ecto's sandbox in tests
+config :chatter, :sql_sandbox, true
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Chrome Driver
+config :wallaby, driver: Wallaby.Chrome
